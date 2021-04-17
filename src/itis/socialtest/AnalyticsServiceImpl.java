f77d7caf8a630c5889056ea6ea94d9fce83f133d
Aidar Ibragimov <ibragimovaidarp@gmail.com>
2021-04-17 11:45:33 +0300

    Done

--- src/itis/socialtest/AnalyticsServiceImpl.java
+++ src/itis/socialtest/AnalyticsServiceImpl.java
@@ -3,25 +3,37 @@ package itis.socialtest;
 import itis.socialtest.entities.Post;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AnalyticsServiceImpl implements AnalyticsService {
     @Override
     public List<Post> findPostsByDate(List<Post> posts, String date) {
-        return null;
+        return posts.stream()
+                .filter(post -> post.getDate().equals(date))
+                .collect(Collectors.toList());
     }
 
     @Override
     public String findMostPopularAuthorNickname(List<Post> posts) {
-        return null;
+        return posts.stream()
+                .collect(Collectors.groupingBy(
+                        post -> post.getAuthor().getNickname(),
+                        Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max((e1, e2) -> e1.getValue().compareTo(e2.getValue())).get().getKey();
     }
 
     @Override
     public Boolean checkPostsThatContainsSearchString(List<Post> posts, String searchString) {
-        return null;
+        return posts.stream().anyMatch(post -> post.getContent().contains(searchString));
     }
 
     @Override
     public List<Post> findAllPostsByAuthorNickname(List<Post> posts, String nick){
-        return null;
+        return posts.stream()
+                .filter(post ->
+                        post.getAuthor().getNickname().equals(nick))
+                .collect(Collectors.toList());
     }
 }
--- src/itis/socialtest/MainClass.java
+++ src/itis/socialtest/MainClass.java
@@ -1,10 +1,15 @@
 package itis.socialtest;
 
 
+import itis.socialtest.entities.Author;
 import itis.socialtest.entities.Post;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Objects;
 
 /*
  * В папке resources находятся два .csv файла.
@@ -35,12 +40,94 @@ public class MainClass {
 
     private AnalyticsService analyticsService = new AnalyticsServiceImpl();
 
-    public static void main(String[] args) {
-        new MainClass().run("", "");
+    public static void main(String[] args) throws IOException {
+        new MainClass().run("src/itis/socialtest/resources/PostDatabase.csv",
+                "src/itis/socialtest/resources/Authors.csv");
     }
 
-    private void run(String postsSourcePath, String authorsSourcePath) {
+    private void run(String postsSourcePath, String authorsSourcePath) throws IOException {
+
+        FileReader postsFileReader = new FileReader(postsSourcePath);
+        BufferedReader bufferedPostsFileReader = new BufferedReader(postsFileReader);
+
+        FileReader authorsFileReader = new FileReader(authorsSourcePath);
+        BufferedReader bufferedAuthorsFileReader = new BufferedReader(authorsFileReader);
 
 
+        List<Author> authorList = new ArrayList<>();
+        String[] authorData = bufferedAuthorsFileReader.readLine().split(", ");
+
+        while (authorData.length > 1) {
+            authorList.add(
+                    new Author(
+                            Long.parseLong(authorData[0]),
+                            authorData[1],
+                            authorData[2]
+                    )
+            );
+            String line = bufferedAuthorsFileReader.readLine();
+            if (Objects.isNull(line)){
+                break;
+            }
+            authorData = line.split(", ");
+        }
+
+
+        List<Post> postList = new ArrayList<>();
+        String[] postData = bufferedPostsFileReader.readLine().split(", ");
+
+        while (postData.length > 1){
+            Long authorId = Long.parseLong(postData[0]);
+            Author author = authorList.stream()
+                    .filter(author_ -> author_.getId().equals(authorId))
+                    .findFirst().orElseThrow(() -> new RuntimeException("Author does not exist"));
+
+            postList.add(
+                    new Post(postData[2],
+                            postData[3],
+                            Long.parseLong(postData[1]),
+                            author
+                    )
+            );
+            String line = bufferedPostsFileReader.readLine();
+            if (Objects.isNull(line)){
+                break;
+            }
+            postData = line.split(", ");
+        }
+
+        postList.stream().forEach(post -> printPost(post));
+
+        AnalyticsService analyticsService = new AnalyticsServiceImpl();
+
+        // Task 2
+        System.out.println("Task 2 output: ");
+        analyticsService.findPostsByDate(postList, "17.04.2021T10:00").stream()
+                .forEach(post -> printPost(post));
+
+        // Task 3
+        System.out.println("Task 3 output: ");
+        analyticsService.findAllPostsByAuthorNickname(postList, "varlamov").stream()
+                .forEach(post -> printPost(post));
+
+        // Task 4
+        System.out.println("Task 4 output: ");
+        System.out.println(analyticsService.checkPostsThatContainsSearchString(postList, "Россия"));
+
+        // Task 5
+        System.out.println("Task 5 output: ");
+        System.out.println(analyticsService.findMostPopularAuthorNickname(postList));
+    }
+
+    private void printPost(Post post){
+        System.out.println(
+                "Author: \n"
+                        + "\t Nickname: "+ post.getAuthor().getNickname() + "\n"
+                        + "\t Id: "+ post.getAuthor().getId() + "\n"
+                        + "\t Birth date: "+ post.getAuthor().getBirthdayDate() + "\n" +
+                "Date: " + post.getDate() + "\n" +
+                "Likes count: " + post.getLikesCount() + "\n" +
+                "Content: " + post.getContent() + "\n"
+        );
     }
 }
